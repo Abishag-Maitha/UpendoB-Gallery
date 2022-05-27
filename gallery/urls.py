4245123e1dd84b0e220b5e92a5235e44ac3fb593
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-28 01:49:31 +0300

    update the urls with the image view url

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,10 +2,15 @@ from django.urls import re_path as url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from .views import *
 
 urlpatterns=[
     url('',views.welcome,name = 'welcome'),
+    url('image_upload/',imageView, name='image_upload'),
+    url('success/', uploadok, name = 'success'),
 ]
+
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
