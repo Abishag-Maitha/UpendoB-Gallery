c7bdcb5621dddd78d1a8a3c777fb9b2254bbb192
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-27 23:41:44 +0300

    create image, location and category models

---
+++ gallery/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = '')
+    name = models.CharField(max_length =50)
+    description = models.TextField(max_length =200, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update_image()
+
+    def search_image(category):
+        category.search_image()
+    
+    
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
