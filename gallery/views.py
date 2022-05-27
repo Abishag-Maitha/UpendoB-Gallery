1ef059ac52b180432215b7c88cfe185c57244c0f
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-28 01:42:35 +0300

    create a view function for the uploads

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
+from gallery.form import ImageForm
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the PendoB Gallery')
+
+
+def imageView(request):
+  
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ImageForm()
+    return render(request, 'uploads.html', {'form' : form})
+  
+  
+def uploadok(request):
+    return HttpResponse(' upload successful')
 
