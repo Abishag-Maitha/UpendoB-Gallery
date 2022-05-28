7d9bd69b159c8e09e4b50b3b73bb9b1a2a7534ce
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-28 17:51:42 +0300

    Create url for home page and image uploads

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,14 +1,14 @@
-from django.urls import re_path as url
+from django.urls import path as url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from .views import *
+
 
 urlpatterns=[
     url('',views.welcome,name = 'welcome'),
-    url('image_upload/',imageView, name='image_upload'),
-    url('success/', uploadok, name = 'success'),
+    url('image_upload/',views.imageView, name='image_upload'),
+    url('success/', views.uploadok, name = 'success'),
 ]
 
 
