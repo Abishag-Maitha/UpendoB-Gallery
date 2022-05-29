fb8dd076f4ebe14a304d473bde1d5d134f1475a4
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-30 00:43:49 +0300

    Add routes for updating image, deleting and saving categories

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from .form import ImageForm
 from .models import Image, Location, Category
+from django.contrib import messages
 
 # Create your views here.
 def welcome(request):
@@ -15,7 +16,7 @@ def imageView(request):
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            # return redirect('success')
+            return redirect(welcome)
     else:
         form = ImageForm()
     return render(request, 'uploads.html', {'form' : form})
@@ -23,14 +24,14 @@ def imageView(request):
 def uploadok(request):
     return HttpResponse(' upload successful')
 
-def search_images(request):
+def search_category(request):
 
-    if 'image' in request.GET and request.GET["image"]:
-        category = request.GET.get("image")
-        searched_images = Image.objects.search_image(category)
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        searched_categories = Image.search_image(category)
         message = f"{category}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_images})
+        return render(request, 'search.html',{"message":message,"images": searched_categories})
 
     else:
         message = "You haven't searched for any category"
@@ -39,3 +40,36 @@ def search_images(request):
 def view_image(request,id):
     image = Image.objects.filter(id=id).first()
     return render(request, 'image_details.html',{'image':image})
+
+def delete_image(request, id):
+    image_deleted=Image.objects.get(id=id)
+    image_deleted.delete()
+    messages.success(request, 'Image successfully deleted!')
+    return redirect(welcome)
+
+def update_image(request, id):
+    value=Image.get_image_by_id(id)
+    form=ImageForm(request.POST or None, request.FILES or None, instance=value)
+    if request.method=='POST':
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'Image updated successfully!')
+            return redirect(view_image, id=value.id)
+    
+    return render (request, 'update.html', {'form':form})
+
+def save_category(request):
+    if 'category' in request.POST and request.POST['category']:
+        name=request.POST.get('category')
+        new_category=Category(name=name)
+        new_category.save()
+        return redirect(welcome)
+
+def save_location(request):
+    if 'location' in request.POST and request.POST['location']:
+        name=request.POST.get('location')
+        results=Location.objects.filter(name__exact=name).first()
+        if results is None:
+            new_location=Location(name=name)
+            new_location.save()
+            return redirect(welcome)
