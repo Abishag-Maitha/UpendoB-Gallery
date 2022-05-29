f236b5ed19cb2bea47d9333c2b4d38d5741b00d4
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-30 00:42:39 +0300

    Added urls to search, update and delete image

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,8 +9,12 @@ urlpatterns=[
     url('',views.welcome, name = 'welcome'),
     url('image_upload/',views.imageView, name='image_upload'),
     url('success/', views.uploadok, name = 'success'),
-    url('search_images/', views.search_images, name='search_images'),
+    url('search_images/', views.search_category, name='search_images'),
     url('image/<int:id>',views.view_image,name='view_image'),
+    url('delete_image/<int:id>', views.delete_image, name='delete_image'),
+    url('update_image/<int:id>', views.update_image, name='update_image'),
+    url('save_category/',views.save_category, name='category'),
+    url('save_location/',views.save_location, name='location'),
 ]
 
 
