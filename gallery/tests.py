7e556fd9f8a178d41e0e39ce725f26655c58c6ad
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-31 00:48:15 +0300

    create tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,6 +1,64 @@
 from django.test import TestCase
+from .models import Image, Category, Location
 
 # Create your tests here.
+class ImageTestClass(TestCase):
 
-# Testing Save Method
+    # Set up method
+    def setUp(self):
+        self.demo= Image(name = 'Demo', decription ='Used to demo a project', location ='Moringa', category='School')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.demo,Image))
+
+   # Testing Save Method
+    def test_save_method(self):
+        self.demo.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+
+#Category Class Tests
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.school= Category(name = 'School')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.school,Category))
+
+   # Testing Save Method
+    def test_save_method(self):
+        self.school.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+#Location Class Tests
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.moringa= Category(name = 'Moringa')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.moringa,Location))
+
+   # Testing Save Method
+    def test_save_method(self):
+        self.moringa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+
+   
+
+
 
